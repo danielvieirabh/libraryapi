080d19bffae16e8e90dc38aa82d314315ff7bb9c
daniel vieira <danielvieiraxbh30@gmail.com>
2026-02-23 22:48:34 -0300

    fix : entidade Autor

--- src/main/java/io/github/cursodanzin/libraryapi/model/Autor.java
+++ src/main/java/io/github/cursodanzin/libraryapi/model/Autor.java
@@ -1,8 +1,71 @@
 package io.github.cursodanzin.libraryapi.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
+@Table(name = "autor", schema = "public") //Schema nao e obrigatorio , pois public e o PADRÃO
+//@Getter //Isso e do Plugin Lombok
+//@Setter
 public class Autor {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
+    @Column(name = "nome", length = 100, nullable = false) //nullabe = false : not null
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false) //nullabe = false : not null
+    private String nacionalidade;
+
+    public Autor() { //Framework exige construtor vazio  , pois o banco de dados faz a consulta dele vazio
+
+    }
+
+    public Autor(UUID id, String nome, LocalDate dataNascimento, String nacionalidade) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.nacionalidade = nacionalidade;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
