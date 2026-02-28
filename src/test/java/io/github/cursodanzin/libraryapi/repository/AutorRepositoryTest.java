b8e26739e22bfeaf26c036cf6e32b7d29abb9205
daniel vieira <danielvieiraxbh30@gmail.com>
2026-02-28 20:39:44 -0300

    fix

--- src/test/java/io/github/cursodanzin/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/cursodanzin/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -35,6 +36,33 @@ public class AutorRepositoryTest {
             autorEncontrado.setDataNascimento(LocalDate.of(1960, 1, 30));
             autorRepository.save(autorEncontrado);
         }
-
     }
+
+    @Test
+    public void listarTest() { //Listar autores da tabela
+        List<Autor> list = autorRepository.findAll();
+        for (Autor lista : list) {
+            System.out.println(lista);
+        }
+    }
+
+    @Test
+    public void countTest() {
+        System.out.println("Contagem de autores: " + autorRepository.count());
+    }
+
+    @Test
+    public void deletePorIdTest() {
+        UUID id = UUID.fromString("fe6e8d43-cc28-4270-b608-4aa7504d94cb");
+        autorRepository.deleteById(id);
+    }
+
+    @Test
+    public void deletePorObjectTest() {
+        UUID id = UUID.fromString("daa2ce86-8ff4-4217-995d-19eedb93ba58");
+        Autor jose = autorRepository.findById(id).get(); //get porque tenho certeza que o autor esta la
+        autorRepository.delete(jose);
+    }
+
+
 }
